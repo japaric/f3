f2ec6ab838ecc3b2ced677db3b3a822414a50f09
Jorge Aparicio <japaricious@gmail.com>
2016-10-08 10:32:05 -0500

    bump compiler-builtins-snapshot, format floats in fpu example

--- examples/fpu.rs
+++ examples/fpu.rs
@@ -9,15 +9,11 @@ extern crate f3;
 
 #[export_name = "main"]
 pub extern "C" fn main() -> ! {
-    let x = black_box(2_f32);
-    let y = black_box(3_f32);
+    let x = black_box(2.5_f32);
+    let y = black_box(3.5_f32);
     let z = black_box(x * y);
 
-    // FIXME(japaric/rustc-builtins#79) there is a problem with the
-    // rustc-builtins (compiler-builtins-snapshot) crate and LTO. Avoid the
-    // issue by printing these values as integers because integer formatting
-    // doesn't need any intrinsic.
-    iprintln!("{} * {} = {}", x as u32, y as u32, z as u32);
+    iprintln!("{} * {} = {}", x, y, z);
 
     loop {}
 }
