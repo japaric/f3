e481d0df376fcbd884b94f7eb01e84f59c025b08
Jorge Aparicio <japaricious@gmail.com>
2016-10-20 01:41:15 -0500

    fix a bug where the first delay was ignored

--- src/delay.rs
+++ src/delay.rs
@@ -16,6 +16,12 @@ pub fn ms(n: u16) {
         // One tick = 1 ms (see `init`)
         tim7.arr.write(|w| w.arr(n));
 
+        // Generate an update event to update the autoreload register
+        tim7.egr.write(|w| w.ug(true));
+
+        // Clear any previous "update" event by clearing the update event flag
+        tim7.sr.write(|w| w);
+
         // CEN: Enable the counter
         tim7.cr1.modify(|_, w| w.cen(true));
 
