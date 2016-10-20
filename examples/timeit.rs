7c38000fa94682c2c30ce16e66c49e433d779eff
Jorge Aparicio <japaricious@gmail.com>
2016-10-20 01:41:41 -0500

    time more operations in the timeit example

--- examples/timeit.rs
+++ examples/timeit.rs
@@ -1,4 +1,4 @@
-//! Timing a section of code
+//! Timing sections of code
 
 #![no_main]
 #![no_std]
@@ -9,13 +9,23 @@ extern crate f3;
 use f3::timeit::FREQUENCY;
 use f3::{delay, timeit};
 
+macro_rules! timeit {
+    ($e:expr) => {
+        let bias = timeit::timeit(|| {});
+        let ticks = timeit::timeit(|| $e) - bias;
+        iprintln!("{} took {} ticks ({} s)",
+                  stringify!($e),
+                  ticks,
+                  ticks as f32 / FREQUENCY as f32);
+    }
+}
+
 #[export_name = "main"]
 pub extern "C" fn main() -> ! {
-    let ticks = timeit::timeit(|| delay::ms(1_000));
-
-    iprintln!("{} ticks ({} s) elapsed",
-              ticks,
-              ticks as f32 / FREQUENCY as f32);
+    timeit!(delay::ms(1_000));
+    timeit!(iprintln!("Hello, world!"));
+    timeit!(iprintln!("{}", 42));
+    timeit!(iprintln!("{}", core::f32::consts::PI));
 
     loop {}
 }
