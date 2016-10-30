335a78c464ac2cdc3cac100a71c05dcbc11ce6cd
Jorge Aparicio <japaricious@gmail.com>
2016-10-29 19:25:10 -0500

    update gen-peripherals.sh

--- gen-peripherals.sh
+++ gen-peripherals.sh
@@ -3,17 +3,28 @@
 set -e
 
 main() {
-    svd2rust -i STM32F30x.svd dbgmcu > src/peripheral/dbgmcu.rs
-    svd2rust -i STM32F30x.svd gpioa > src/peripheral/gpio.rs
+    local svd=STM32F30x.svd
+
+    svd2rust -i $svd tim6 > src/peripheral/btim.rs
+    sed -i 's/\(pub struct \)Tim6/\1BTim/' src/peripheral/btim.rs
+
+    svd2rust -i $svd dbgmcu > src/peripheral/dbgmcu.rs
+
+    svd2rust -i $svd gpioa > src/peripheral/gpio.rs
     sed -i 's/\(pub struct Gpio\)a/\1/' src/peripheral/gpio.rs
-    svd2rust -i STM32F30x.svd i2c1 > src/peripheral/i2c.rs
+
+    svd2rust -i $svd tim2 > src/peripheral/gptim.rs
+    sed -i 's/\(pub struct \)Tim2/\1GpTim/' src/peripheral/gptim.rs
+
+    svd2rust -i $svd i2c1 > src/peripheral/i2c.rs
     sed -i 's/\(pub struct I2c\)1/\1/' src/peripheral/i2c.rs
-    svd2rust -i STM32F30x.svd rcc > src/peripheral/rcc.rs
-    svd2rust -i STM32F30x.svd spi1 > src/peripheral/spi.rs
+
+    svd2rust -i $svd rcc > src/peripheral/rcc.rs
+
+    svd2rust -i $svd spi1 > src/peripheral/spi.rs
     sed -i 's/\(pub struct Spi\)1/\1/' src/peripheral/spi.rs
-    svd2rust -i STM32F30x.svd tim6 > src/peripheral/tim.rs
-    sed -i 's/\(pub struct Tim\)6/\1/' src/peripheral/tim.rs
-    svd2rust -i STM32F30x.svd usart1 > src/peripheral/usart.rs
+
+    svd2rust -i $svd usart1 > src/peripheral/usart.rs
     sed -i 's/\(pub struct Usart\)1/\1/' src/peripheral/usart.rs
 
     set +e
