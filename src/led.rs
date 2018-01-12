cec02d8fda402cfe099147ba942c4dc4106da4c9
Jorge Aparicio <jorge@japaric.io>
2018-01-12 23:43:42 +0100

    bring back Leds and Direction

--- src/led.rs
+++ src/led.rs
@@ -1,3 +1,5 @@
+use core::ops;
+
 use hal::prelude::*;
 
 use hal::gpio::GPIOE::{PE10, PE11, PE12, PE13, PE14, PE15, PE8, PE9, PEx};
@@ -11,6 +13,80 @@ pub type LD7 = PE11<Output<PushPull>>;
 pub type LD8 = PE14<Output<PushPull>>;
 pub type LD9 = PE12<Output<PushPull>>;
 pub type LD10 = PE13<Output<PushPull>>;
+
+pub enum Direction {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
+}
+
+pub struct Leds {
+    leds: [Led; 8],
+}
+
+impl Leds {
+    pub fn new(n: LD3, ne: LD5, e: LD7, se: LD9, s: LD10, sw: LD8, w: LD6, nw: LD4) -> Self {
+        Leds {
+            leds: [
+                n.into(),
+                ne.into(),
+                e.into(),
+                se.into(),
+                s.into(),
+                sw.into(),
+                w.into(),
+                nw.into(),
+            ],
+        }
+    }
+}
+
+impl ops::Deref for Leds {
+    type Target = [Led];
+
+    fn deref(&self) -> &[Led] {
+        &self.leds
+    }
+}
+
+impl ops::DerefMut for Leds {
+    fn deref_mut(&mut self) -> &mut [Led] {
+        &mut self.leds
+    }
+}
+
+impl ops::Index<usize> for Leds {
+    type Output = Led;
+
+    fn index(&self, i: usize) -> &Led {
+        &self.leds[i]
+    }
+}
+
+impl ops::Index<Direction> for Leds {
+    type Output = Led;
+
+    fn index(&self, d: Direction) -> &Led {
+        &self.leds[d as usize]
+    }
+}
+
+impl ops::IndexMut<usize> for Leds {
+    fn index_mut(&mut self, i: usize) -> &mut Led {
+        &mut self.leds[i]
+    }
+}
+
+impl ops::IndexMut<Direction> for Leds {
+    fn index_mut(&mut self, d: Direction) -> &mut Led {
+        &mut self.leds[d as usize]
+    }
+}
 
 pub struct Led {
     pex: PEx<Output<PushPull>>,
