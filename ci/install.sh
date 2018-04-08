a4fedc0590730d640636ed05265709d04b60c002
Jorge Aparicio <jorge@japaric.io>
2018-04-08 22:50:22 +0200

    s/Xargo/Cargo/

--- ci/install.sh
+++ ci/install.sh
@@ -1,21 +1,9 @@
 set -euxo pipefail
 
 main() {
-    # This fetches latest stable release of Xargo
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/xargo \
-                    | cut -d/ -f3 \
-                    | grep -E '^v[0.1.0-9.]+$' \
-                    | sort --version-sort \
-                    | tail -n1)
-
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/xargo \
-           --tag $tag \
-           --target x86_64-unknown-linux-musl
-
-    rustup component add rust-src
+    if [ $TARGET = thumbv7em-none-eabihf ]; then
+        rustup target add $TARGET
+    fi
 }
 
 main
